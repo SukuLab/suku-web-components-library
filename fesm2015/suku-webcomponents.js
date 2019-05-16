b55900194e703c4546b0910dfe66472682234c46
dhanushreemc <mcdhanu1988@gmail.com>
2019-05-16 07:45:25 +0000

    updated

--- fesm2015/suku-webcomponents.js
+++ fesm2015/suku-webcomponents.js
@@ -4573,8 +4573,8 @@ class SukuImageWidgetComponent {
 SukuImageWidgetComponent.decorators = [
     { type: Component, args: [{
                 selector: 'suku-image-widget',
-                template: "<div class=\"product-img\">\n    <img src=\"{{imageSrc}}\"\n      class=\"ImgRes imgBorder rounded-0\" alt=\"product-01\" />\n  </div>",
-                styles: [".product-img{width:auto;max-width:256px;max-height:158px;height:auto;float:left;text-align:center;line-height:172px;position:relative;z-index:10}.ImgRes{max-width:100%;max-height:158px}.imgBorder{border:1px solid #f2f2f2!important}"]
+                template: "<div class=\"product-img imgBorder\" [ngClass]=\"{'shine': !imageSrc}\">\n  <img *ngIf=\"imageSrc\" src=\"{{imageSrc}}\"\n      class=\"ImgRes rounded-0 center\" alt=\"product-01\" />\n  </div>",
+                styles: [".product-img{min-height:158px;width:256px;float:left;text-align:center;line-height:158px;position:relative;z-index:10}.ImgRes{max-width:100%;max-height:140px}.imgBorder{border:1px solid #f2f2f2!important}.center{text-align:center}@-webkit-keyframes placeholderShimmer{0%{background-position:-468px 0}100%{background-position:468px 0}}@keyframes placeholderShimmer{0%{background-position:-468px 0}100%{background-position:468px 0}}.shine{background:0 0/800px 200px no-repeat #f6f7f8;background-image:linear-gradient(to right,#f6f7f8 0,#edeef1 20%,#f6f7f8 40%,#f6f7f8 100%);display:inline-block;position:relative;-webkit-animation-duration:1s;animation-duration:1s;-webkit-animation-fill-mode:forwards;animation-fill-mode:forwards;-webkit-animation-iteration-count:infinite;animation-iteration-count:infinite;-webkit-animation-name:placeholderShimmer;animation-name:placeholderShimmer;-webkit-animation-timing-function:linear;animation-timing-function:linear}"]
             }] }
 ];
 /** @nocollapse */
