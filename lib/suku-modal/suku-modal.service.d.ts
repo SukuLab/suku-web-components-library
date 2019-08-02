368ce058303367bb4515d93b837d0d1e7f5f1f84
dhanushreemc <mcdhanu1988@gmail.com>
2019-08-02 14:33:17 +0000

    updated

--- lib/suku-modal/suku-modal.service.d.ts
+++ lib/suku-modal/suku-modal.service.d.ts
@@ -5,21 +5,7 @@ export declare class SukuModalService {
     confirmationDialogWidth: any;
     confirmationDialogHeight: any;
     confirmationDialogClose: any;
-    onDialogCloseInterest: EventEmitter<any>;
-    onDialogCloseList: EventEmitter<any>;
-    onDialogClosePlaceBid: EventEmitter<any>;
-    onDialogCloseAcceptBid: EventEmitter<any>;
-    onDialogCloseSendPayment: EventEmitter<any>;
-    onDialogCloseDeleteList: EventEmitter<any>;
-    onDialogCloseCancelBid: EventEmitter<any>;
-    onDialogCloseUpdateInterest: EventEmitter<any>;
+    onDialogClose: EventEmitter<any>;
     constructor(dialogService: MatDialog);
-    openConfirmationDialogInterest(data?: any): void;
-    openConfirmationDialogList(data?: any): void;
-    openConfirmationDialogPlaceBid(data?: any): void;
-    openConfirmationDialogAcceptBid(data?: any): void;
-    openConfirmationDialogSendPayment(data?: any): void;
-    openConfirmationDialogDeleteList(data?: any): void;
-    openConfirmationDialogCancelBid(data?: any): void;
-    openConfirmationDialogUpdateInterest(data?: any): void;
+    openConfirmationDialog(data?: any): void;
 }
