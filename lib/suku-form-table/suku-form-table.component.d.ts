a668d727508b3581b73d668152b79bea4e54866c
dhanushreemc <mcdhanu1988@gmail.com>
2019-09-13 00:04:43 +0000

    updated

--- lib/suku-form-table/suku-form-table.component.d.ts
+++ lib/suku-form-table/suku-form-table.component.d.ts
@@ -1,7 +1,8 @@
-import { OnInit } from '@angular/core';
+import { OnInit, EventEmitter } from '@angular/core';
 export declare class SukuFormTableComponent implements OnInit {
     j: any;
     _items: any[];
+    _itemsKey: any;
     editable: any[];
     titleOneId: any;
     titleOneSize: any;
@@ -14,12 +15,14 @@ export declare class SukuFormTableComponent implements OnInit {
     headerWeight: any;
     colorPallete: string[];
     status: string[];
+    enableEditIndex: any;
     typeKey: any;
     defaultCount: number;
     items: any[];
     selectionKey: any;
     highlighterKey: any;
     patchKey: any;
+    hiddenKey: any[];
     enableHighlighter: boolean;
     enableControls: boolean;
     enableSelectAll: boolean;
@@ -27,12 +30,17 @@ export declare class SukuFormTableComponent implements OnInit {
     keyData: any;
     controlsSize: any;
     controlCustomClass: any;
+    save: EventEmitter<{}>;
+    remove: EventEmitter<{}>;
+    submitData: EventEmitter<{}>;
     constructor();
     ngOnInit(): void;
     addTable(val: any, editIndex: any): void;
     submit(): void;
-    edit(i: any): void;
-    remove(i: any): void;
+    edit(i: any, item: any): void;
+    removedata(i: any, item: any): void;
     checkValid(list: any): boolean;
     selectAllAction(): void;
+    chkHiddenKey(key: any): boolean;
+    sendData(): void;
 }
