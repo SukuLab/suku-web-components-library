a60a437a81f848f676024850248c5f444de84c08
dhanushreemc <mcdhanu1988@gmail.com>
2019-11-04 08:15:39 +0000

    updated

--- lib/suku-claims-widget/suku-claims-widget.component.d.ts
+++ lib/suku-claims-widget/suku-claims-widget.component.d.ts
@@ -1,4 +1,4 @@
-import { OnInit } from '@angular/core';
+import { OnInit, EventEmitter } from '@angular/core';
 export declare class SukuClaimsWidgetComponent implements OnInit {
     claims: {
         name: string;
@@ -11,6 +11,7 @@ export declare class SukuClaimsWidgetComponent implements OnInit {
     tooltipInfo: string;
     positionTooltipRight: any;
     positionTooltipBottom: any;
+    action: EventEmitter<{}>;
     constructor();
     ngOnInit(): void;
 }
