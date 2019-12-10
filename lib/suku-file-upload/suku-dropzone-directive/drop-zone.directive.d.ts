b274e46a5123d93bc7a92e994796ab8c4260816e
parashur <parashuram.b@comorin.co>
2019-12-10 12:26:10 +0530

    icon size update

--- lib/suku-file-upload/suku-dropzone-directive/drop-zone.directive.d.ts
+++
@@ -1,9 +0,0 @@
-import { EventEmitter } from '@angular/core';
-export declare class SukuDropZoneDirective {
-    dropped: EventEmitter<FileList>;
-    hovered: EventEmitter<boolean>;
-    constructor();
-    onDrop($event: any): void;
-    onDragOver($event: any): void;
-    onDragLeave($event: any): void;
-}
